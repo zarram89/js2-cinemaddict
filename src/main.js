2df33ce77c20e2ad3799a56d77fe0ea1d4f7b16f
Ramil Zaripov <zarram89@gmail.com>
2024-09-13 20:10:07 +0500

    Отрисует первый компонент

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
+import HeaderProfileView from "./view/header-profile-view";
 
+import { render } from "./render";
+
+const siteHeaderElement = document.querySelector('.header');
+
+render(new HeaderProfileView(), siteHeaderElement);
