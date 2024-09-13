d7b63930d27986ae5b81c2a7b8186b553f36017e
Ramil Zaripov <zarram89@gmail.com>
2024-09-13 18:57:21 +0500

    Подготавливает проект

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
-const path = require('path');
-const CopyPlugin = require("copy-webpack-plugin");
+const path = require('path'); // Подключаем модуль Node.js для работы с путями на компьютере
+const CopyPlugin = require("copy-webpack-plugin"); // Подключаем плагин
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/main.js', // Указывает точку входа - наш главный JavaScript-файл проекта
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build'),
-    clean: true,
+    filename: 'bundle.js', // Задаём название итоговому бандлу
+    path: path.resolve(__dirname, 'build'), // Указываем, куда нужно положить бандл после создания
+    clean: true,  // Отмечаем, что перед сохранением обновлённого бандла старые файлы нужно удалить
   },
-  devtool: 'source-map',
+  devtool: 'source-map', // Аквтивируем генерацию source-maps
   plugins: [
     new CopyPlugin({
-      patterns: [{ from: 'public' }],
+      patterns: [{ from: 'public' }], // Добавляем плагин в сборку и указываем директорию, откуда нужно скопировать файлы
     }),
   ],
   module: {
     rules: [
         {
-          test: /\.js$/,
-          exclude: /(node_modules)/,
-          use: ['babel-loader']
+          test: /\.js$/, // Указываем регулярным выражением, что преобразовывать нужно только JavaScript-файлы
+          exclude: /(node_modules)/, // Уточняем, что файлы из директории node_modules трогать не нужно
+          use: ['babel-loader'] // Указываем, какой лоадер использовать
         }
     ]
   }
